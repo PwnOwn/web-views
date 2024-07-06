452daa7a5327c3b53e4f0ed3774c8519011e9113
PwnOwn <116953490+PwnOwn@users.noreply.github.com>
2024-07-06 21:39:17 +0800

    update github address

--- metadata/index.ts
+++ metadata/index.ts
@@ -7,9 +7,9 @@ export const siteDescription = 'algorithm is good idea.'
 export const include = ['note', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/PwnOwn/web-views'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://discord.gg/t9wu6z4z'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
